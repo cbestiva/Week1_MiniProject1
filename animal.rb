d510dd7051e9de9b8eec5e6d12412995197856e4
cbestiva <cbestiva@gmail.com>
2014-01-12 12:27:16 -0800

    commit phase 1: create animals

---
+++ animal.rb
@@ -0,0 +1,23 @@
+class Animal
+	attr_accessor :a_name, :a_age, :a_gender, :a_species, :toys
+
+	def initialize(a_name, a_age, a_gender, a_species)
+		@a_name = a_name
+		@a_age = a_age
+		@a_gender = a_gender
+		@a_species = a_species
+		@toys = []
+	end
+
+end
+
+#Phase 1 Test (create animals)
+Mochi = Animal.new("Mochi", 5, "female", "Maltese")
+Mochi.toys << "ball"
+Mochi.toys << "rope"
+# puts Mochi.a_name #=> "Mochi"
+# puts Mochi.a_age #=> 5
+# puts Mochi.toys #=> "ball" "rope"
+# puts Mochi.toys[0] #=> "ball"
+
+Russell = Animal.new("Russell", 3, "male", "Maltese")
