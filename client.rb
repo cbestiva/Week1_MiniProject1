87ed355cb6cad723705d758d2fdece7de11f9e15
cbestiva <cbestiva@gmail.com>
2014-01-12 12:45:39 -0800

    commit phase 1: create clients

---
+++ client.rb
@@ -0,0 +1,18 @@
+class Client
+	attr_accessor :c_name, :c_age, :num_of_children, :pets
+
+	def initialize(c_name, c_age, num_of_children)
+		@c_name = c_name
+		@c_age = c_age
+		@num_of_children = num_of_children
+		@pets = {}
+	end
+
+	def display_pets
+		pets.values.join("\n")
+	end
+
+end
+
+#Phase 1 Test (create clients)
+Char = Client.new("Char", 29, 0)
